2364b86e85613c32a45ea57f24ee4e6763cf5959
Mark Smalley <mark@smalley.my>
2014-10-16 22:09:47 +0100

    added QRs

--- htdocs/_libs/js/web.js
+++ htdocs/_libs/js/web.js
@@ -3,6 +3,7 @@ var blockchains = {
     init: function()
     {
         blockchains.nav();
+        blockchains.qr();
     },
     nav: function()
     {
@@ -11,6 +12,38 @@ var blockchains = {
             {
                 event.stopPropagation();
             }
+        });
+    },
+    modal: function(title, content, id)
+    {
+        var selector = $('#default-modal');
+        if(id) selector = $('#'+id);
+        if(title) $(selector).find('.modal-title').html(title);
+        if(content) $(selector).find('.modal-body').html(content);
+        $(selector).modal('show');
+    },
+    qr: function()
+    {
+        $('body').on('click', '.btn-qr', function(e)
+        {
+            e.preventDefault();
+            var content = $(this).attr('data-content');
+            var qr = '<p class="qr" data-content="'+content+'"></p>';
+            blockchains.modal(content, '<div class="qr-holder">'+qr+'</div>');
+        });
+        $('.modal').on('show.bs.modal', function(e)
+        {
+            $(this).find('p.qr').each(function()
+            {
+                if($(this).find('img').length > 0)
+                {
+                    $(this).find('img').remove();
+                }
+                $(this).qrcode({
+                    render: 'image',
+                    text: $(this).attr('data-content')
+                });
+            });
         });
     }
 };
